8353f3f75d97c84e3a7a7a661cfc78af7f1559d1
hg.wow060118 <yang.fengrui@immomo.com>
2019-01-10 20:01:27 +0800

    text

--- src/main/java/com/yfr/thiesis/ThiesisApplication.java
+++ src/main/java/com/yfr/thiesis/ThiesisApplication.java
@@ -3,13 +3,19 @@ package com.yfr.thiesis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.yfr.thiesis.dao")
-public class ThiesisApplication {
+public class ThiesisApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ThiesisApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(ThiesisApplication.class);
     }
 
 }
